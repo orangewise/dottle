3e42d838425aee1ef6faf1fecb2d8788bffe04d9
Mac Heller-Ogden <mac@machellerogden.com>
2019-09-05 14:42:43 -0500

    WIP

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,26 +3,29 @@
 import test from 'ava';
 import { read } from '..';
 
-test.skip('works', async t => {
-    const input = JSON.stringify({
-        "StartAt": "a",
-        "States": {
-            "a": {
-                "Type": "Pass",
-                "Next": "b"
-            },
-            "b": {
-                "Type": "Pass",
-                "End": true
-            }
-        }
-    });
+const fixtures = {
+    basic: JSON.stringify(require('./fixtures/basic')),
+    parallel: JSON.stringify(require('./fixtures/parallel'))
+};
+
+test('basic', async t => {
     const expected = [
-        'digraph { a -> b }'
+        'digraph { node [shape=record]; a -> b; b -> c; c -> d; }'
     ];
     t.plan(expected.length);
     let i = 0;
-    for await (const result of read(input)) {
+    for await (const result of read(fixtures.basic)) {
+        t.deepEqual(result, expected[i++]);
+    }
+});
+
+test.skip('parallel', async t => {
+    const expected = [
+        'digraph { node [shape=record]; a -> b; subgraph b { b1a -> b1b; b2a -> b2b; } b -> c; }'
+    ];
+    t.plan(expected.length);
+    let i = 0;
+    for await (const result of read(fixtures.parallel)) {
         t.deepEqual(result, expected[i++]);
     }
 });
