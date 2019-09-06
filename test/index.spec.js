89331a44fb50cb0c17571a939f6a5ba690326ebd
Mac Heller-Ogden <mac@machellerogden.com>
2019-09-05 22:41:51 -0500

    WIP

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,6 +2,11 @@
 
 import test from 'ava';
 import { read } from '..';
+import styles from '../lib/styles';
+const {
+    digraph:digraphStyles,
+    subgraph:subgraphStyles
+} = styles;
 
 const fixtures = {
     basic: JSON.stringify(require('./fixtures/basic')),
@@ -10,7 +15,17 @@ const fixtures = {
 
 test('basic', async t => {
     const expected = [
-        'digraph { node [shape=record]; a -> b; b -> c; c -> d; }'
+`digraph {
+
+    ${digraphStyles}
+
+    start -> a;
+    a -> b;
+    b -> c;
+    c -> d;
+    d -> end;
+
+}`.replace(/\s\s+/g, ' ')
     ];
     t.plan(expected.length);
     let i = 0;
@@ -19,22 +34,34 @@ test('basic', async t => {
     }
 });
 
-test.skip('parallel', async t => {
+test('parallel', async t => {
     const expected = [
 `digraph {
-    compound=true;
-    subgraph cluster_b1 {
+
+    ${digraphStyles}
+
+    start [shape=circle fillcolor="#ffda75" style="dashed,filled"];
+    end [shape=circle fillcolor="#ffda75" style="dashed,filled"];
+
+    start -> a;
+
+    subgraph cluster_b {
+
+        color=grey; style="dashed,rounded";
+
         b1a -> b1b;
-    }
-    subgraph cluster_b2 {
         b2a -> b2b;
     }
-    a -> b;
-    b -> b1a [lhead=cluster_b1];
-    b -> b2a [lhead=cluster_b2];
-    b1b -> c [ltail=cluster_b1];
-    b2b -> c [ltail=cluster_b2];
-}`
+
+    a -> b1a;
+    a -> b2a;
+
+    b1b -> c;
+    b2b -> c;
+
+    c -> end;
+
+}`.replace(/\s\s+/g, ' ')
     ];
     t.plan(expected.length);
     let i = 0;
